6e09cea6044a7b8da24247d7e820b168b982de94
omonra <omonra@483a4429-297d-4d5f-a489-5c5d2c303994>
2003-11-09 03:34:54 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.lablz.com/hosts/trunk/hosts@2632 483a4429-297d-4d5f-a489-5c5d2c303994

--- localSearchResults.jsp
+++ localSearchResults.jsp
@@ -128,64 +128,10 @@
 </div>
 
 <div id="textdiv4">
-<script language="JavaScript">
-  function onRecChange() {
-    if (document.scheduleForm.rec.value == 'week') {
-      document.getElementById("titleDiv").innerHTML = '<b>Day:</b>';
-      document.getElementById("valueDiv").innerHTML = '<select name="weekDay">' +
-                           '<option value="Mon">Monday</option>' +
-                           '<option value="Tue">Tuesday</option>' +
-                           '<option value="Wed">Wednesday</option>' +
-                           '<option value="Thu">Thursday</option>' +
-                           '<option value="Fri">Friday</option>' +
-                           '<option value="Sat">Saturday</option>' +
-                           '<option value="Sun">Sunday</option>' +
-                           '</select>';
-    } else if (document.scheduleForm.rec.value == 'month') {
-      document.getElementById("titleDiv").innerHTML = '<b>Day:</b>';
-      document.getElementById("valueDiv").innerHTML = '<select name="day">' +
-                           '<option>01</option>' +
-                           '<option>02</option>' +
-                           '<option>03</option>' +
-                           '<option>04</option>' +
-                           '<option>05</option>' +
-                           '<option>06</option>' +
-                           '<option>07</option>' +
-                           '<option>08</option>' +
-                           '<option>09</option>' +
-                           '<option>10</option>' +
-                           '<option>11</option>' +
-                           '<option>12</option>' +
-                           '<option>13</option>' +
-                           '<option>14</option>' +
-                           '<option>15</option>' +
-                           '<option>16</option>' +
-                           '<option>17</option>' +
-                           '<option>18</option>' +
-                           '<option>19</option>' +
-                           '<option>20</option>' +
-                           '<option>21</option>' +
-                           '<option>22</option>' +
-                           '<option>23</option>' +
-                           '<option>24</option>' +
-                           '<option>25</option>' +
-                           '<option>26</option>' +
-                           '<option>27</option>' +
-                           '<option>28</option>' +
-                           '<option>29</option>' +
-                           '<option>30</option>' +
-                           '<option>31</option>' +
-                           '</select>';
-    } else {
-      document.getElementById("titleDiv").innerHTML = '';
-      document.getElementById("valueDiv").innerHTML = '';
-    }
-  }
-</script>
 <form name="scheduleForm" action="page2schedule" method="GET">
   <table cellpadding="5"><tr><td>
     <table border="1" cellpadding="5">
-      <tr><td><b>Name:</b></td><td><input name="name"/></td></tr>
+      <tr><td><b>Subject:</b></td><td><input name="name"/></td></tr>
       <tr><td><b>Recurrency:</b></td>
       <td>
         <select name="rec" onchange="onRecChange()">
---
+++ menu.js
@@ -0,0 +1,182 @@
+var tabShow=0;
+function hidepoptext() {
+	poptext = textdiv.style;
+	if (tabShow == 1) {
+		poptext.visibility = "hidden";
+		tabShow = 0;
+		return;
+	}
+	if (tabShow == 0) {
+		if (tabShow1 == 1) {
+			hidepoptext1();
+		}
+		else if (tabShow2 == 1) {
+			hidepoptext2();
+		}
+		else if (tabShow3 == 1) {
+			hidepoptext3();
+		}			
+		else if (tabShow4 == 1) {
+			hidepoptext4();
+		}
+		poptext.visibility = "visible";
+		tabShow = 1;
+	}
+}
+
+var tabShow1=0;
+function hidepoptext1() {
+	poptext1 = textdiv1.style;
+	if (tabShow1 == 1) {
+		poptext1.visibility = "hidden";
+		tabShow1 = 0;
+		return;
+	}
+	if (tabShow1 == 0) {
+		if (tabShow == 1) {
+			hidepoptext();
+		}
+		else if (tabShow2 == 1) {
+			hidepoptext2();
+		}
+		else if (tabShow3 == 1) {
+			hidepoptext3();
+		}			
+		else if (tabShow4 == 1) {
+			hidepoptext4();
+		}
+		poptext1.visibility = "visible";
+		tabShow1 = 1;
+	}
+}
+
+var tabShow2=0;
+function hidepoptext2() {
+	poptext2 = textdiv2.style;
+	if (tabShow2 == 1) {
+		poptext2.visibility = "hidden";
+		tabShow2 = 0;
+		return;
+	}
+	if (tabShow2 == 0) {
+		if (tabShow == 1) {
+			hidepoptext();
+		}
+		else if (tabShow1 == 1) {
+			hidepoptext1();
+		}
+		else if (tabShow3 == 1) {
+			hidepoptext3();
+		}			
+		else if (tabShow4 == 1) {
+			hidepoptext4();
+		}
+		poptext2.visibility = "visible";
+		tabShow2 = 1;
+	}
+}
+
+var tabShow3=0;
+function hidepoptext3() {
+	poptext3 = textdiv3.style;
+	if (tabShow3 == 1) {
+		poptext3.visibility = "hidden";
+		tabShow3 = 0;
+		return;
+	}
+	if (tabShow3 == 0) {
+		if (tabShow == 1) {
+			hidepoptext();
+		}
+		else if (tabShow1 == 1) {
+			hidepoptext1();
+		}
+		else if (tabShow2 == 1) {
+			hidepoptext2();
+		}
+		else if (tabShow4 == 1) {
+			hidepoptext4();
+		}
+		poptext3.visibility = "visible";
+		tabShow3 = 1;
+	}
+}
+
+var tabShow4=0;
+function hidepoptext4() {
+	poptext4 = textdiv4.style;
+	if (tabShow4 == 1) {
+		poptext4.visibility = "hidden";
+		tabShow4 = 0;
+		return;
+	}
+	if (tabShow4 == 0) {
+		if (tabShow == 1) {
+			hidepoptext();
+		}
+		else if (tabShow1 == 1) {
+			hidepoptext1();
+		}
+		else if (tabShow2 == 1) {
+			hidepoptext2();
+		}
+		else if (tabShow3 == 1) {
+			hidepoptext3();
+		}
+		poptext4.visibility = "visible";
+		tabShow4 = 1;
+	}
+}
+
+function onRecChange() {
+  if (document.scheduleForm.rec.value == 'week') {
+    document.getElementById("titleDiv").innerHTML = '<b>Day:</b>';
+    document.getElementById("valueDiv").innerHTML = '<select name="weekDay">' +
+                         '<option value="Mon">Monday</option>' +
+                         '<option value="Tue">Tuesday</option>' +
+                         '<option value="Wed">Wednesday</option>' +
+                         '<option value="Thu">Thursday</option>' +
+                         '<option value="Fri">Friday</option>' +
+                         '<option value="Sat">Saturday</option>' +
+                         '<option value="Sun">Sunday</option>' +
+                         '</select>';
+  } else if (document.scheduleForm.rec.value == 'month') {
+    document.getElementById("titleDiv").innerHTML = '<b>Day:</b>';
+    document.getElementById("valueDiv").innerHTML = '<select name="day">' +
+                         '<option>01</option>' +
+                         '<option>02</option>' +
+                         '<option>03</option>' +
+                         '<option>04</option>' +
+                         '<option>05</option>' +
+                         '<option>06</option>' +
+                         '<option>07</option>' +
+                         '<option>08</option>' +
+                         '<option>09</option>' +
+                         '<option>10</option>' +
+                         '<option>11</option>' +
+                         '<option>12</option>' +
+                         '<option>13</option>' +
+                         '<option>14</option>' +
+                         '<option>15</option>' +
+                         '<option>16</option>' +
+                         '<option>17</option>' +
+                         '<option>18</option>' +
+                         '<option>19</option>' +
+                         '<option>20</option>' +
+                         '<option>21</option>' +
+                         '<option>22</option>' +
+                         '<option>23</option>' +
+                         '<option>24</option>' +
+                         '<option>25</option>' +
+                         '<option>26</option>' +
+                         '<option>27</option>' +
+                         '<option>28</option>' +
+                         '<option>29</option>' +
+                         '<option>30</option>' +
+                         '<option>31</option>' +
+                         '</select>';
+  } else {
+    document.getElementById("titleDiv").innerHTML = '';
+    document.getElementById("valueDiv").innerHTML = '';
+  }
+}
