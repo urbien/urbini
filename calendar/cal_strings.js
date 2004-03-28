b3d372403795be92c5fbea2487ebd5f13498d079
omonra <omonra@483a4429-297d-4d5f-a489-5c5d2c303994>
2004-03-28 05:24:21 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.lablz.com/hosts/trunk/hosts@3135 483a4429-297d-4d5f-a489-5c5d2c303994

---
+++ calendar/cal_strings.js
@@ -0,0 +1,14 @@
+var ARR_STRINGS = {
+	'long_days' : ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+	'short_days' : ['Sun', 'Mon', 'Tue', 'Wen', 'Thu', 'Fri', 'Sat'],
+	'long_month' : ['January','February','March','April','May','June','July','August','September','October','November','December'],
+	'short_month' : ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+	'bad_month' : 'Parsing error: unknown month "%month_name"',
+	'need_form_name' : 'Form name is required parameter of draw method',
+	'form_not_found' : "Form with name '%form_name' can't be found in the document",
+	'max_date' : 'Dates after %max_date are not allowed',
+	'min_date' : 'Dates before %min_date are not allowed',
+	'not_meet' : 'Warning: Input date does not meet input date format',
+	'forbidden' : 'Date %forbidden_date is forbidden',
+	'not_format' : 'Notice : Error of input date format'
+}
---
+++ calendar/cals_init.js
@@ -0,0 +1,5 @@
+// initialization block for all calendars on the page
+// A_CALENDARS array contains references to each calendar instance
+for (var n = 0; n < A_CALENDARS.length; n++) {
+  A_CALENDARS[n].create();
+}
