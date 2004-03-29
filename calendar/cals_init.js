afdff4e5862074475624b4a790e6d03f30ef2def
omonra <omonra@483a4429-297d-4d5f-a489-5c5d2c303994>
2004-03-29 01:27:49 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.lablz.com/hosts/trunk/hosts@3144 483a4429-297d-4d5f-a489-5c5d2c303994

--- calendar/cals_init.js
+++ calendar/cals_init.js
@@ -1,7 +1,9 @@
 // initialization block for all calendars on the page
 // A_CALENDARS array contains references to each calendar instance
+try {
 if (A_CALENDARS != null) {
   for (var n = 0; n < A_CALENDARS.length; n++) {
     A_CALENDARS[n].create();
   }
 }
+} catch (e) {}
