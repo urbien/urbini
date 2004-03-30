410376bd0705e5a895ba8bad36273d2abad5a460
gv <gv@483a4429-297d-4d5f-a489-5c5d2c303994>
2004-03-30 16:57:12 +0000

    more robust var checking
    git-svn-id: svn+ssh://svn.lablz.com/hosts/trunk/hosts@3163 483a4429-297d-4d5f-a489-5c5d2c303994

--- calendar/cals_init.js
+++ calendar/cals_init.js
@@ -1,9 +1,11 @@
 // initialization block for all calendars on the page
 // A_CALENDARS array contains references to each calendar instance
+
 try {
-if (A_CALENDARS != null) {
-  for (var n = 0; n < A_CALENDARS.length; n++) {
-    A_CALENDARS[n].create();
+  if (typeof A_CALENDARS != 'undefined' && A_CALENDARS != null) {
+    for (var n = 0; n < A_CALENDARS.length; n++) {
+      A_CALENDARS[n].create();
+    }
   }
 }
-} catch (e) {}
+catch (e) {}
