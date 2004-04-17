f6634859e5a4b1c22bf267134f41c51e6bb8a25e
gv <gv@483a4429-297d-4d5f-a489-5c5d2c303994>
2004-04-17 01:52:34 +0000

    moved out of GanttChart code
    git-svn-id: svn+ssh://svn.lablz.com/hosts/trunk/hosts@3251 483a4429-297d-4d5f-a489-5c5d2c303994

---
+++ gantt/dependencies.js
@@ -0,0 +1,54 @@
+var clicker = false;
+var counter = 0;
+var last    = "";
+
+function showDeps(source, dep, numprojects) {
+  // clean all lines
+  if (clicker) {
+    if (last != source) 
+      counter = 0;
+    counter++;
+    for (var i = 1; i <= ((numprojects - 1)*3); i++) {
+      alert("clean " + i);
+      document.getElementById('line' + i).from = "0,0";
+      document.getElementById('line' + i).to   = "0,0";
+    }
+    k = 1;
+  }
+  if (!clicker && last == source) {
+    if (counter%2 != 2) {
+      var i = document.getElementById(source).offsetLeft;
+      var j = document.getElementById(source).offsetTop;
+      j += 10;
+      var td = document.getElementById("thisOne").offsetLeft;
+      var i1 = document.getElementById(dep).offsetLeft;
+      var j1 = document.getElementById(dep).offsetTop;
+      j1 += 10;
+      document.getElementById('line' + k).from     = "" + i  + "," + (j-4)
+      document.getElementById('line' + k).to       = "" + td + "," + (j-4);
+      document.getElementById('line' + (k+1)).from = "" + td + "," + (j-4);
+      document.getElementById('line' + (k+1)).to   = "" + td + "," + (j1-4);
+      document.getElementById('line' + (k+2)).from = "" + td + "," + (j1-4);
+      document.getElementById('line' + (k+2)).to   = "" + i1 + "," + (j1-4);
+      k += 3;
+    }
+  }
+  if (last != source) {
+      var i  = document.getElementById(source).offsetLeft;
+      var j  = document.getElementById(source).offsetTop;
+      j += 10;
+      var td = document.getElementById("thisOne").offsetLeft;
+      var i1 = document.getElementById(dep).offsetLeft;
+      var j1 = document.getElementById(dep).offsetTop;
+      j1 += 10;
+      document.getElementById('line' + k).from     = "" + i  + "," + (j-4)
+      document.getElementById('line' + k).to       = "" + td + "," + (j-4);
+      document.getElementById('line' + (k+1)).from = "" + td + "," + (j-4);
+      document.getElementById('line' + (k+1)).to   = "" + td + "," + (j1-4);
+      document.getElementById('line' + (k+2)).from = "" + td + "," + (j1-4);
+      document.getElementById('line' + (k+2)).to   = "" + i1 + "," + (j1-4);
+      k += 3;
+  }
+  last = source;
+  clicker = false;
+}
