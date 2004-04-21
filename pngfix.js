26c862e439bad0d490e999ecc393eb319a7ba9e0
gv <gv@483a4429-297d-4d5f-a489-5c5d2c303994>
2004-04-21 02:52:05 +0000

    better alignment
    git-svn-id: svn+ssh://svn.lablz.com/hosts/trunk/hosts@3287 483a4429-297d-4d5f-a489-5c5d2c303994

--- pngfix.js
+++ pngfix.js
@@ -13,7 +13,8 @@ function correctPNG()
 		 var imgID = (img.id) ? "id='" + img.id + "' " : ""
 		 var imgClass = (img.className) ? "class='" + img.className + "' " : ""
 		 var imgTitle = (img.title) ? "title='" + img.title + "' " : "title='" + img.alt + "' "
-		 var imgStyle = "display:inline-block;" + img.style.cssText 
+		 //var imgStyle = "display:inline-block;" + img.style.cssText 
+                 var imgStyle = img.style.cssText 
 		 if (img.align == "left") imgStyle = "float:left;" + imgStyle
 		 if (img.align == "right") imgStyle = "float:right;" + imgStyle
 		 if (img.align == "middle") imgStyle = "float:middle;" + imgStyle
