18218415fb5efbe46ef544937f91cd306b44a8bd
gv <gv@483a4429-297d-4d5f-a489-5c5d2c303994>
2004-10-23 21:03:38 +0000

    added dynamic form fields add/remove
    git-svn-id: svn+ssh://svn.lablz.com/hosts/trunk/hosts@4110 483a4429-297d-4d5f-a489-5c5d2c303994

--- forms.js
+++ forms.js
@@ -71,3 +71,45 @@ function trim(s) {
 function displayFullText(div, text) {
   document.getElementById(div).innerHTML = text;
 }
+
+/*************************  Form fields adding/removing *******************/
+function addField(form, fieldType, fieldName, fieldValue) {
+  if (document.getElementById) {
+    var input = document.createElement('INPUT');
+      if (document.all) { // what follows should work
+                          // with NN6 but doesn't in M14
+        input.type = fieldType;
+        input.name = fieldName;
+        input.value = fieldValue;
+      }
+      else if (document.getElementById) { // so here is the
+                                          // NN6 workaround
+        input.setAttribute('type', fieldType);
+        input.setAttribute('name', fieldName);
+        input.setAttribute('value', fieldValue);
+      }
+    form.appendChild(input);
+  }
+}
+function getField (form, fieldName) {
+  if (!document.all)
+    return form[fieldName];
+  else  // IE has a bug not adding dynamically created field
+        // as named properties so we loop through the elements array
+    for (var e = 0; e < form.elements.length; e++)
+      if (form.elements[e].name == fieldName)
+        return form.elements[e];
+  return null;
+}        
+function removeField (form, fieldName) {
+  var field = getField (form, fieldName);
+  if (field && !field.length)
+    field.parentNode.removeChild(field);
+}
+function toggleField (form, fieldName, value) {
+  var field = getField (form, fieldName);
+  if (field)
+    removeField (form, fieldName);
+  else
+    addField (form, 'hidden', fieldName, value);
+}
