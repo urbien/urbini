b7df96fbe3241fcec23c75ab9703a88ffc81aeb8
omonra <omonra@483a4429-297d-4d5f-a489-5c5d2c303994>
2004-12-22 04:04:27 +0000

    added cookie functions
    git-svn-id: svn+ssh://svn.lablz.com/hosts/trunk/hosts@4408 483a4429-297d-4d5f-a489-5c5d2c303994

--- chat/chat.js
+++ chat/chat.js
@@ -10,3 +10,31 @@ function openChatWindow(title, resUrl) {
   var url = 'chatRoom?title=' + title + '&referer=' + resUrl;
   window.open(url, 'chat','width='+w+',height='+h+',top='+ch+',left='+cw+', menubar=no, status=0, location=no, toolbar=no, scrollbars=no, status=no, resizable=yes');
 }
+
+function createCookie(name, value, days) {
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime()+(days*24*60*60*1000));
+    var expires = "; expires="+date.toGMTString();
+  } else {
+    var expires = "";
+  }
+  document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') 
+      c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) 
+      return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+function eraseCookie(name) {
+  createCookie(name,"",-1);
+}
