4c4f25d19744e016f116da5958841e6a8686b449
gv <gv@483a4429-297d-4d5f-a489-5c5d2c303994>
2005-05-07 00:41:45 +0000

    moved code to browserEventHandling
    git-svn-id: svn+ssh://svn.lablz.com/hosts/trunk/hosts@5034 483a4429-297d-4d5f-a489-5c5d2c303994

--- iframe.js
+++
@@ -1,7 +0,0 @@
-// add onload functions only if inside iframe
-if (window.parent != window) {
-  function onLoadPopup() {
-    parent.frameLoaded[window.name] = true;
-  }
-  addEvent(window, 'load', function() {setTimeout(onLoadPopup, 0);}, false);
-}
