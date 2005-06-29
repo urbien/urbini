bbe9d24eb4b1aae349bf08f20954bd39a7452c08
nik <nik@483a4429-297d-4d5f-a489-5c5d2c303994>
2005-06-29 21:56:47 +0000

    resizing bug - Gene: it is very annoying that chat window opens in one size and then resizes to another
    git-svn-id: svn+ssh://svn.lablz.com/hosts/trunk/hosts@5242 483a4429-297d-4d5f-a489-5c5d2c303994

--- chat/chat.js
+++ chat/chat.js
@@ -1,11 +1,11 @@
-var w = 640/3, h = 480/3, cw = w/2, ch = h/2;
+//var w = 640/3, h = 480/3, cw = w/2, ch = h/2;
 
 function openChatWindow(title, resUrl) {
   if (window.screen) {
-    w = Math.floor(screen.availWidth*2/3);
-    h = Math.floor(screen.availHeight*2/3);
+    w = Math.floor(screen.availWidth*3/5);
+    h = Math.floor(screen.availHeight*3/5);
     cw = Math.floor(screen.availWidth/6);
-    ch = Math.floor((screen.availHeight)/6);
+    ch = Math.floor(screen.availHeight/6);
   }
   var url = 'chatRoom?title=' + title + '&referer=' + resUrl;
   window.open(url, 'chat','width='+w+',height='+h+',top='+ch+',left='+cw+', menubar=no, status=0, location=no, toolbar=no, scrollbars=no, status=no, resizable=yes');
