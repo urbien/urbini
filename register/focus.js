7329dde1edd5dae25e10cfd5732c3801a7f2322a
gv <gv@483a4429-297d-4d5f-a489-5c5d2c303994>
2006-01-09 07:14:52 +0000

    IE complains that element on which to focus is not active - reproes on Schedule page if session expired and u cancel one of the treatments
    git-svn-id: svn+ssh://svn.lablz.com/hosts/trunk/hosts@6234 483a4429-297d-4d5f-a489-5c5d2c303994

--- register/focus.js
+++ register/focus.js
@@ -3,8 +3,8 @@
     if (f) {
       var u = f.elements['j_username'];
       if (u && u.type && u.type != 'hidden')
-         u.focus();
-    } 
+         try {u.focus();} catch (e) {}
+    }
     return true;
   }
   addEvent(window, 'load', function() {setTimeout(focusOnUserName, 0);}, false);
