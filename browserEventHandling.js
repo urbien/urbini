75eb02eff564c94a128437c875047315aabfff9b
gv <gv@483a4429-297d-4d5f-a489-5c5d2c303994>
2006-06-21 21:12:51 +0000

    optimizations - use document.images and .links;
    git-svn-id: svn+ssh://svn.lablz.com/hosts/trunk/hosts@7079 483a4429-297d-4d5f-a489-5c5d2c303994

--- browserEventHandling.js
+++ browserEventHandling.js
@@ -86,28 +86,6 @@
       return null;
     }
 
-    /**
-     * remove modifier, like ctrl_y
-     */
-    function removeModifier(url, param) {
-      var urlStr = url.href;
-      var idx = urlStr.indexOf(param);
-      if (idx == -1)
-        return url;
-
-      var len = param.length;
-      if (urlStr.charAt(idx - 1) == '&') {
-        idx--;
-        len++;
-      }
-
-      var uBefore = urlStr.substring(0, idx);
-      var uAfter  = urlStr.substring(idx + len);
-      urlStr = uBefore + uAfter;
-      url.href = urlStr;
-      // alert('before='+uBefore + ', after=' + uAfter);
-    }
-
     //***** upon iframe loading inform the parent
     function onLoadPopup() {
       if (parent && parent.frameLoaded)
@@ -116,13 +94,18 @@
 
     //***** Add smartlistbox handlers
     function addHandlers() {
+//      Packages.java.lang.System.out.println('onLoad 0: ' + new Date());
       if (window.parent != window) {
         onLoadPopup();
         return;
       }
+//      Packages.java.lang.System.out.println('onLoad 1: ' + new Date());
       interceptLinkClicks();
+//      Packages.java.lang.System.out.println('onLoad 2: ' + new Date());
       initListBoxes(null);
+//      Packages.java.lang.System.out.println('onLoad 3: ' + new Date());
       uiFocus();
+//      Packages.java.lang.System.out.println('onLoad 4: ' + new Date());
 //      if (typeof searchHighlighting != 'undefined')
 //        searchHighlighting();
 //      if (typeof replaceAllTooltips != 'undefined')
@@ -152,8 +135,8 @@
 	      return;
 
       var elemLen = elemId.length;
-      if (elemId.indexOf("_boolean",          elemLen - "_boolean".length) != -1  ||
-          elem.id.indexOf("_boolean_refresh", elemLen - "_boolean_refresh".length) != -1) {
+      if (elemId.indexOf("_boolean",         elemLen - "_boolean".length) != -1  ||
+          elemId.indexOf("_boolean_refresh", elemLen - "_boolean_refresh".length) != -1) {
         addEvent(elem, 'click', changeBoolean, false);
         elem.style.cursor = 'pointer';
       }
@@ -212,107 +195,6 @@
       }
       //tooltipMouseOut0(target);           // remove and ...
       //tooltipMouseOver0(target);          // repaint the tooltip on this boolean icon
-    }
-
-    function initListBoxes(div) {
-      var images;
-      if (div)
-        images = div.getElementsByTagName('img');
-      else
-        images = document.images;
-      for (var i=0; i<images.length; i++) {
-        var image = images[i];
-        if (image.id.indexOf("_filter", image.id.length - "_filter".length) != -1) {
-          if (typeof listboxOnClick == 'undefined')
-            continue;
-          addEvent(image, 'click', listboxOnClick, false); // add handler to smartlistbox images
-        }
-        else
-          addBooleanToggle(image);
-        replaceTooltip(div, image);
-      }
-
-      // 1. add handler to autocomplete filter form text fields
-      // 2. save initial values of all fields
-      if (typeof autoComplete == 'undefined')
-        return;
-      var forms;
-      if (div)
-        forms = div.getElementsByTagName('form');
-      else
-        forms = document.forms;
-      formInitialValues = new Array(forms.length);
-      for (var i=0; i<forms.length; i++) {
-        var form = forms[i];
-        var initialValues = new Array(form.elements.length);
-        formInitialValues[form.name] = initialValues;
-        if (form.id != 'filter')
-          continue;
-        addEvent(form, 'submit', popupOnSubmit, false);
-        for (j=0; j<form.elements.length; j++) {
-          var elem = form.elements[j];
-          replaceTooltip(div, elem);
-          initialValues[elem.name] = elem.value;
-          if (elem.type && elem.type.toUpperCase() == 'TEXT' &&  // only on TEXT fields
-              elem.id) {                                         // and those that have ID
-            if (document.all) // in IE - some keys (like backspace) work only on keydown
-              addEvent(elem, 'keydown',  autoCompleteOnKeyDown,     false);
-            else
-              addEvent(elem, 'keypress', autoComplete,              false);
-            addEvent(elem, 'focus',      autoCompleteOnFocus,       false);
-            addEvent(elem, 'blur',       autoCompleteOnBlur,        false);
-            addEvent(elem, 'mouseout',   autoCompleteOnMouseout,    false);
-            //addEvent(elem, 'change',   onFormFieldChange, false);
-            //addEvent(elem, 'blur',     onFormFieldChange, false);
-            //addEvent(elem, 'click',    onFormFieldClick,  false);
-          }
-          else if (elem.type && elem.type.toUpperCase() == 'TEXTAREA') {
-            var rows = elem.attributes['rows'];
-            var cols = elem.attributes['cols'];
-            if (rows)
-              initialValues[elem.name + '.attributes.rows'] = rows.value;
-            if (cols)
-              initialValues[elem.name + '.attributes.cols'] = cols.value;
-            if (!elem.value || elem.value == '') {
-              elem.setAttribute('rows', 1);
-              elem.setAttribute('cols', 10);
-              //elem.attributes['cols'].value = 10;
-              addEvent(elem, 'focus', textAreaOnFocus,  false);
-              addEvent(elem, 'blur',  textAreaOnBlur,   false);
-            }
-          }
-          else  {
-             //         alert(elem.name + ", " + elem.type + ", " + elem.id + ", " + elem.valueType);
-          }
-        }
-      }
-    }
-
-    function uiFocus(div) {
-      if (!div)
-        div = document;
-
-      var fields = div.getElementsByTagName('input');
-      var firstField;
-
-      for (var i=0; i<fields.length; i++) {
-        var u = fields[i];
-        if (u && u.type && u.type != 'hidden') {
-          if (!firstField) {
-            firstField = u;
-          }
-          if (u.id && (u.id == 'uiFocus' || u.id.indexOf('_uiFocus') != -1)) {
-            u.focus(); // in IE (at least in IE6) first focus() is lost for some reason - we are forced to issue another focus()
-            u.focus();
-            return true;
-          }
-        }
-      }
-      if (firstField && div != document) {
-        firstField.focus();
-        firstField.focus(); // second time for IE
-      }
-      return false;
     }
 
     var formInitialValues;
