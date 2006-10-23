ab2f07e608ffe5fdb85d206f26aaeafd4f154b4a
gv <gv@483a4429-297d-4d5f-a489-5c5d2c303994>
2006-10-23 01:40:53 +0000

    test for presense of JS in browser
    git-svn-id: svn+ssh://svn.lablz.com/hosts/trunk/hosts@7401 483a4429-297d-4d5f-a489-5c5d2c303994

--- register/focus.js
+++ register/focus.js
@@ -1,6 +1,8 @@
   function focusOnUserName() {
     var f = document.forms['loginform'];
     if (f) {
+      var jstest = f.elements['.jstest'];
+      jstest.value = "ok"; // server will know that JavaScript worked
       var u = f.elements['j_username'];
       if (u && u.type && u.type != 'hidden')
          try {u.focus();} catch (e) {}
