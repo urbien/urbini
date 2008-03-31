c58f53daaf579881373ce3f148d07dd213496b27
omonra <omonra@483a4429-297d-4d5f-a489-5c5d2c303994>
2008-03-31 18:10:52 +0000

    Mobile.autologin
    git-svn-id: svn+ssh://svn.lablz.com/hosts/trunk/hosts@9268 483a4429-297d-4d5f-a489-5c5d2c303994

--- menu.js
+++ menu.js
@@ -2915,15 +2915,17 @@ var BrowserRuntime = {
       this.eventObjects['key']            = jsiKeyEvent;
       needHandler = true;
     }
-    if (needHandler) {
-      this.log('adding keydown');
-      addEvent(document, 'keypress', this.eventArrived, false); // this fake key event is programatically injected by android LablZ adapter
-      addEvent(document, 'keydown', this.eventArrived, false); // this fake key event is programatically injected by android LablZ adapter
-      addEvent(document, 'keyup', this.eventArrived, false); // this fake key event is programatically injected by android LablZ adapter
-    }
-    this.eventManager.readyForEvents();
     if (this.eventManager.isEventsViaTimer()) {
       setInterval(this.eventArrived, 1000);
+    }
+    else {
+      if (needHandler) {
+        this.log('adding keydown');
+        addEvent(document, 'keypress', this.eventArrived, false); // this fake key event is programatically injected by android LablZ adapter
+        addEvent(document, 'keydown', this.eventArrived, false); // this fake key event is programatically injected by android LablZ adapter
+        addEvent(document, 'keyup', this.eventArrived, false); // this fake key event is programatically injected by android LablZ adapter
+      }      
+      this.eventManager.readyForEvents();
     }
 
     if (typeof jsiView != 'undefined')
@@ -2962,7 +2964,9 @@ var BrowserRuntime = {
       handlers = new Array();
       this.eventHandlers[eventType] = handlers;
     }
+    BrowserRuntime.log('adding handler: ' + handler);
     handlers[handlers.length] = handler;
+    BrowserRuntime.log('added handler: ' + handlers[handlers.length - 1]);
   },
 
   /******* private members ********/
@@ -2974,22 +2978,24 @@ var BrowserRuntime = {
     if (e) {
       //var code = getKeyCode(e);
       var code = e.keyCode;
-      this.log('got key event: ' + code);
+      BrowserRuntime.log('got key event: ' + code);
       if (code != 39) // process only fake key event
         return;
     }
-    var hasEvent = this.eventManager.hasEvent();
+    var hasEvent = BrowserRuntime.eventManager.hasEvent();
     if (hasEvent) {
-      var eventType = this.eventManager.getEventType();
-      var handlers = this.eventHandlers[eventType];
-      var eventObject = this.eventObjects[eventType];
+      var eventType = BrowserRuntime.eventManager.getEventType();
+      var handlers = BrowserRuntime.eventHandlers[eventType];
+      var eventObject = BrowserRuntime.eventObjects[eventType];
       for (var handler in handlers) {
+        if (handler == null || handler == 0)
+          continue;
+        BrowserRuntime.log(handler);
         if (handler)
           handler(eventObject);
       }
       var rc = stopEventPropagation(e);
-      this.log("calling jsiEventManager.popEvent()");
-      this.eventManager.popEvent();
+      BrowserRuntime.eventManager.popEvent();
       return rc;
     }
   }
@@ -3000,15 +3006,49 @@ var Mobile = {
   urlToDivs  : null,
   browsingHistory :  null,
   browsingHistoryPos : 0,
-
-  init: function() {
+  
+  init: function(event) {
     if (!Popup.mobile)
       return;
     BrowserRuntime.addEventHandler('xmpp', this.onChatMessage);
     BrowserRuntime.addEventHandler('key',  this.onKey);
     addEvent(document.body, 'click',  this.onClick, false);
+    this.onPageLoad();
   },
 
+  onPageLoad: function(newUrl) {
+    Mobile.autoLogin(newUrl);
+  },
+  
+  autoLogin : function(url) {
+    if (!url)
+      url = document.location.href;
+    if (url.indexOf('user-login.html') == -1)
+      return;
+    BrowserRuntime.log('autoLogin: ' + url);
+    var loginform = document.forms['loginform'];    
+    var jstest = loginform.elements['.jstest'];
+    jstest.value = "ok"; // server will know that JavaScript worked
+    var pw = loginform.elements['j_password'];
+    var username = loginform.elements['j_username'];
+    pw.value = 'mark';
+    username.value = 'mark';
+    
+    if (typeof hash == 'undefined') {
+      var script = '<script src="register/hashScript.js" type="text/javascript" language="JavaScript"></script>';
+      div = document.createElement("DIV");
+      document.body.appendChild(div);
+      div.innerHTML = script; 
+    }
+    
+    hash(loginform, 'j_security_check');
+    
+    var params = getFormFilters(loginform, true, new Array());
+    //var urlFields = document.location.split('?');
+    var url = loginform.action;
+    postRequest(event, url, params, null, null, function() {}, true);    
+  },
+  
   onKey: function(e) {
     if (e.keyCode == 4) // BrowserRuntime.keyboard.getBackButtonCode())
       this.oneStep(null, -1);
@@ -3208,8 +3248,9 @@ var Mobile = {
     var idx = url.lastIndexOf('/');
     url = url.substring(0, idx + 1) + 'm' + url.substring(idx);
     postRequest(e, url, urlParts[1], div, link, loadPage);
-    function loadPage(event, div, hotspot, content) {
+    function loadPage(event, div, hotspot, content, contentUrl) {
       setInnerHtml(div, content);
+      Mobile.onPageLoad(contentUrl);
       if (BrowserRuntime.xmpp) {
         var d = document.getElementById('lastIMtime');
         if (d) {
@@ -6542,6 +6583,7 @@ function postRequest(event, url, parameters, div, hotspot, callback, noCache) {
     if (http_request.readyState != 4) // ignore for now: 0-Unintialized,
                                       // 1-Loading, 2-Loaded, 3-Interactive
       return;
+    BrowserRuntime.log('got back on postrequest for ' + url);
     if (!Popup.mobile  &&  !Popup.penBased  &&  !addLineItem)
       loadingCueFinish();
     var location;
@@ -6562,17 +6604,18 @@ function postRequest(event, url, parameters, div, hotspot, callback, noCache) {
       else
         status = 400;
     }
+    BrowserRuntime.log('got back on postrequest, status ' + status);
     if (status == 200 && url.indexOf('FormRedirect') != -1) { // POST that did not cause redirect - it means it had a problem - repaint dialog with err msg
       frameLoaded[frameId] = true;
       openAjaxStatistics(event, http_request);
       BrowserRuntime.view.setProgress(100);
-      callback(clonedEvent, div, hotspot, http_request.responseText);
+      callback(clonedEvent, div, hotspot, http_request.responseText, url);
     }
     else if (status == 200) {
       frameLoaded[frameId] = true;
       openAjaxStatistics(event, http_request);
       BrowserRuntime.view.setProgress(100);
-      callback(clonedEvent, div, hotspot, http_request.responseText);
+      callback(clonedEvent, div, hotspot, http_request.responseText, url);
     }
     else if (status == 302) {
       try {location = http_request.getResponseHeader('Location');} catch(exception) {}
@@ -6607,6 +6650,7 @@ function postRequest(event, url, parameters, div, hotspot, callback, noCache) {
 
       var paintInPage;
       try { paintInPage = http_request.getResponseHeader('X-Paint-In-Page');} catch (exc) {}
+      BrowserRuntime.log('got back on postrequest, paintinpage ' + paintInPage);
       if (paintInPage && paintInPage == 'false') {
         document.location = location;  // reload full page
       }
@@ -6650,6 +6694,7 @@ function postRequest(event, url, parameters, div, hotspot, callback, noCache) {
     // below 2 line commented - made IE wait with ~1 minute timeout
     // http_request.setRequestHeader("Connection", "close");
     BrowserRuntime.view.setProgress(10);
+    parameters += '&X-Ajax=y'; // webkit does not send custom headers
     http_request.send(parameters);
   }
   // use GET due to Browser bugs
@@ -9911,3 +9956,205 @@ function addBeforeProcessing(tbodyId, subject, event) {
   return retCode;
 }
 */
+
+
+/*********************** hashScript.js **********************/
+/*
+ * A JavaScript implementation of the RSA Data Security, Inc. MD5 Message
+ * Digest Algorithm, as defined in RFC 1321.
+ * Copyright (C) Paul Johnston 1999 - 2000.
+ * Updated by Greg Holt 2000 - 2001.
+ * See http://pajhome.org.uk/site/legal.html for details.
+ */
+
+/*
+ * Convert a 32-bit number to a hex string with ls-byte first
+ */
+var hex_chr = "0123456789abcdef";
+function rhex(num)
+{
+  str = "";
+  for(j = 0; j <= 3; j++)
+    str += hex_chr.charAt((num >> (j * 8 + 4)) & 0x0F) +
+           hex_chr.charAt((num >> (j * 8)) & 0x0F);
+  return str;
+}
+
+/*
+ * Convert a string to a sequence of 16-word blocks, stored as an array.
+ * Append padding bits and the length, as described in the MD5 standard.
+ */
+function str2blks_MD5(str)
+{
+  nblk = ((str.length + 8) >> 6) + 1;
+  blks = new Array(nblk * 16);
+  for(i = 0; i < nblk * 16; i++) blks[i] = 0;
+  for(i = 0; i < str.length; i++)
+    blks[i >> 2] |= str.charCodeAt(i) << ((i % 4) * 8);
+  blks[i >> 2] |= 0x80 << ((i % 4) * 8);
+  blks[nblk * 16 - 2] = str.length * 8;
+  return blks;
+}
+
+/*
+ * Add integers, wrapping at 2^32. This uses 16-bit operations internally
+ * to work around bugs in some JS interpreters.
+ */
+function add(x, y)
+{
+  var lsw = (x & 0xFFFF) + (y & 0xFFFF);
+  var msw = (x >> 16) + (y >> 16) + (lsw >> 16);
+  return (msw << 16) | (lsw & 0xFFFF);
+}
+
+/*
+ * Bitwise rotate a 32-bit number to the left
+ */
+function rol(num, cnt)
+{
+  return (num << cnt) | (num >>> (32 - cnt));
+}
+
+/*
+ * These functions implement the basic operation for each round of the
+ * algorithm.
+ */
+function cmn(q, a, b, x, s, t)
+{
+  return add(rol(add(add(a, q), add(x, t)), s), b);
+}
+function ff(a, b, c, d, x, s, t)
+{
+  return cmn((b & c) | ((~b) & d), a, b, x, s, t);
+}
+function gg(a, b, c, d, x, s, t)
+{
+  return cmn((b & d) | (c & (~d)), a, b, x, s, t);
+}
+function hh(a, b, c, d, x, s, t)
+{
+  return cmn(b ^ c ^ d, a, b, x, s, t);
+}
+function ii(a, b, c, d, x, s, t)
+{
+  return cmn(c ^ (b | (~d)), a, b, x, s, t);
+}
+
+/*
+ * Take a string and return the hex representation of its MD5.
+ */
+function MD5(str)
+{
+  x = str2blks_MD5(str);
+  var a =  1732584193;
+  var b = -271733879;
+  var c = -1732584194;
+  var d =  271733878;
+
+  for(i = 0; i < x.length; i += 16)
+  {
+    var olda = a;
+    var oldb = b;
+    var oldc = c;
+    var oldd = d;
+
+    a = ff(a, b, c, d, x[i+ 0], 7 , -680876936);
+    d = ff(d, a, b, c, x[i+ 1], 12, -389564586);
+    c = ff(c, d, a, b, x[i+ 2], 17,  606105819);
+    b = ff(b, c, d, a, x[i+ 3], 22, -1044525330);
+    a = ff(a, b, c, d, x[i+ 4], 7 , -176418897);
+    d = ff(d, a, b, c, x[i+ 5], 12,  1200080426);
+    c = ff(c, d, a, b, x[i+ 6], 17, -1473231341);
+    b = ff(b, c, d, a, x[i+ 7], 22, -45705983);
+    a = ff(a, b, c, d, x[i+ 8], 7 ,  1770035416);
+    d = ff(d, a, b, c, x[i+ 9], 12, -1958414417);
+    c = ff(c, d, a, b, x[i+10], 17, -42063);
+    b = ff(b, c, d, a, x[i+11], 22, -1990404162);
+    a = ff(a, b, c, d, x[i+12], 7 ,  1804603682);
+    d = ff(d, a, b, c, x[i+13], 12, -40341101);
+    c = ff(c, d, a, b, x[i+14], 17, -1502002290);
+    b = ff(b, c, d, a, x[i+15], 22,  1236535329);
+
+    a = gg(a, b, c, d, x[i+ 1], 5 , -165796510);
+    d = gg(d, a, b, c, x[i+ 6], 9 , -1069501632);
+    c = gg(c, d, a, b, x[i+11], 14,  643717713);
+    b = gg(b, c, d, a, x[i+ 0], 20, -373897302);
+    a = gg(a, b, c, d, x[i+ 5], 5 , -701558691);
+    d = gg(d, a, b, c, x[i+10], 9 ,  38016083);
+    c = gg(c, d, a, b, x[i+15], 14, -660478335);
+    b = gg(b, c, d, a, x[i+ 4], 20, -405537848);
+    a = gg(a, b, c, d, x[i+ 9], 5 ,  568446438);
+    d = gg(d, a, b, c, x[i+14], 9 , -1019803690);
+    c = gg(c, d, a, b, x[i+ 3], 14, -187363961);
+    b = gg(b, c, d, a, x[i+ 8], 20,  1163531501);
+    a = gg(a, b, c, d, x[i+13], 5 , -1444681467);
+    d = gg(d, a, b, c, x[i+ 2], 9 , -51403784);
+    c = gg(c, d, a, b, x[i+ 7], 14,  1735328473);
+    b = gg(b, c, d, a, x[i+12], 20, -1926607734);
+
+    a = hh(a, b, c, d, x[i+ 5], 4 , -378558);
+    d = hh(d, a, b, c, x[i+ 8], 11, -2022574463);
+    c = hh(c, d, a, b, x[i+11], 16,  1839030562);
+    b = hh(b, c, d, a, x[i+14], 23, -35309556);
+    a = hh(a, b, c, d, x[i+ 1], 4 , -1530992060);
+    d = hh(d, a, b, c, x[i+ 4], 11,  1272893353);
+    c = hh(c, d, a, b, x[i+ 7], 16, -155497632);
+    b = hh(b, c, d, a, x[i+10], 23, -1094730640);
+    a = hh(a, b, c, d, x[i+13], 4 ,  681279174);
+    d = hh(d, a, b, c, x[i+ 0], 11, -358537222);
+    c = hh(c, d, a, b, x[i+ 3], 16, -722521979);
+    b = hh(b, c, d, a, x[i+ 6], 23,  76029189);
+    a = hh(a, b, c, d, x[i+ 9], 4 , -640364487);
+    d = hh(d, a, b, c, x[i+12], 11, -421815835);
+    c = hh(c, d, a, b, x[i+15], 16,  530742520);
+    b = hh(b, c, d, a, x[i+ 2], 23, -995338651);
+
+    a = ii(a, b, c, d, x[i+ 0], 6 , -198630844);
+    d = ii(d, a, b, c, x[i+ 7], 10,  1126891415);
+    c = ii(c, d, a, b, x[i+14], 15, -1416354905);
+    b = ii(b, c, d, a, x[i+ 5], 21, -57434055);
+    a = ii(a, b, c, d, x[i+12], 6 ,  1700485571);
+    d = ii(d, a, b, c, x[i+ 3], 10, -1894986606);
+    c = ii(c, d, a, b, x[i+10], 15, -1051523);
+    b = ii(b, c, d, a, x[i+ 1], 21, -2054922799);
+    a = ii(a, b, c, d, x[i+ 8], 6 ,  1873313359);
+    d = ii(d, a, b, c, x[i+15], 10, -30611744);
+    c = ii(c, d, a, b, x[i+ 6], 15, -1560198380);
+    b = ii(b, c, d, a, x[i+13], 21,  1309151649);
+    a = ii(a, b, c, d, x[i+ 4], 6 , -145523070);
+    d = ii(d, a, b, c, x[i+11], 10, -1120210379);
+    c = ii(c, d, a, b, x[i+ 2], 15,  718787259);
+    b = ii(b, c, d, a, x[i+ 9], 21, -343485551);
+
+    a = add(a, olda);
+    b = add(b, oldb);
+    c = add(c, oldc);
+    d = add(d, oldd);
+  }
+  return rhex(a) + rhex(b) + rhex(c) + rhex(d);
+}
+
+function computeRequestDigest(form) {
+//     request-digest  = KD ( H(A1), nonce-value ":" H(A2) )
+//
+//      A1  = H( username-value ":" realm-value ":" passwd)
+//      A2  = Method ":" digest-uri-value
+
+    if (!form.realm)
+      throw new Error('form input field is missing: realm');
+    if (!form.digest_uri)
+      throw new Error('form input field is missing: digest_uri');
+    if (!form.challenge)
+      throw new Error('form input field is missing: challenge');
+    //alert("JAVA SCRIPT: j_username=" + form.j_username.value + ', realm=' + form.realm.value + ', j_password=' + form.j_password.value + ', digest_uri=' + form.digest_uri.value + ', challenge='  + form.challenge.value);
+    var A1 = MD5(form.j_username.value + ":" +
+                 form.realm.value      + ":" +
+                 form.j_password.value);
+
+    var A2 = MD5("POST" + ":" + form.digest_uri.value);
+
+    var request_digest = MD5(A1 + ":" + form.challenge.value + ":" + A2);
+    var url = request_digest + "&realm=" + escape(form.realm.value) + "&nonce=" + form.challenge.value + "&digest_uri=" + escape(form.digest_uri.value);
+    return url;
+}
+
