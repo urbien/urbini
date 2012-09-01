960b5be193b7a14d9902a6675e67ee764ab489c9
Mark Vayngrib <mark.vayngrib@lablz.com>
2012-09-01 20:40:56 +0800

    importing movie theaters, better importing of movies and cast

--- register/focus.js
+++ register/focus.js
@@ -3,6 +3,18 @@
     if (f) {
       var jstest = f.elements['.jstest'];
       jstest.value = "ok"; // server will know that JavaScript worked
+      if (typeof window.Cordova != 'undefined' || typeof window.Phonegap != 'undefined') {
+        var cordova = document.createElement("input");
+        cordova.type = "hidden";
+        cordova.value = "yes";
+        f.appendChild(cordova); // add it to the form
+
+        var appId = document.createElement("input");
+        appId.type = "hidden";
+        appId.value = getAppId();
+        f.appendChild(appId); // add it to the form
+      }
+      
       var u = f.elements['j_username'];
       if (u && u.type && u.type != 'hidden')
          try {
