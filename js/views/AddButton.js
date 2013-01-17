ef295bc5c3404f847892473225799a4dc09f8aaf
Mark Vayngrib <mark.vayngrib@lablz.com>
2013-01-17 14:54:44 +0800

    addButton

---
+++ js/views/AddButton.js
@@ -0,0 +1,41 @@
+define([
+  'globals',
+  'cache!underscore', 
+  'cache!templates',
+  'cache!utils',
+  'cache!events',
+  'cache!views/BasicView'
+], function(G, _, Templates, U, Events, BasicView) {
+  return BasicView.extend({
+    template: 'addButtonTemplate',
+    events: {
+      'click #addBtn': 'add'
+    },
+    initialize: function(options) {
+      _.bindAll(this, 'render', 'add');
+      this.constructor.__super__.initialize.apply(this, arguments);
+      this.template = _.template(Templates.get(this.template));
+      return this;
+    },
+    add: function(e) {
+      Events.stopEvent(e);
+//      Events.trigger('back');
+//      window.history.back();
+      var colParams = U.getQueryParams(this.collection);
+      colParams['-makeId'] = G.nextId();
+      this.router.navigate('make/' + encodeURIComponent(this.vocModel.type) + (_.size(colParams) ? '?' + $.param(colParams) : ''), {trigger: true, replace: false});
+      return this;
+    },
+    render: function(options) {
+      if (!this.template)
+        return this;
+      
+      if (typeof options !== 'undefined' && options.append)
+        this.$el.append(this.template());
+      else
+        this.$el.html(this.template());
+      
+      return this;
+    }
+  });
+});
