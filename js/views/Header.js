caac702909ab5e6353e4d2afd6e42b0e84e9f32e
Aur3 Admin <admin@lablz.com>
2013-02-08 11:15:19 -0500

    publish button for Apps

--- js/views/Header.js
+++ js/views/Header.js
@@ -6,8 +6,9 @@ define([
   'templates',
   'events', 
   'utils',
+  'views/PublishButton',
   'views/BasicView'
-], function(G, $, _, Backbone, Templates, Events, U, BasicView) {
+], function(G, $, _, Backbone, Templates, Events, U, PublishButton, BasicView) {
   return BasicView.extend({
     template: 'headerTemplate',
     initialize: function(options) {
@@ -87,6 +88,9 @@ define([
       var r = this.buttons.right;
       r && this.makeWidgets(r, {domEl: 'li', id: '#headerUl'}); //, css: 'ui-btn-right'});
       
+      if (this.doPublish) {
+        this.publish = new PublishButton({el: $('div#publishBtn', this.el), model: this.resource}).render();
+      }
       if (G.currentUser.guest) {
         var log = this.buttons.log;
         log && this.makeWidgets(log, {domEl: 'li', id: '#headerUl'}); //, css: 'ui-btn-right'});
