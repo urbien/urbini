bd2e3e5c4e591f14e4e5c384fe4693d1aed194dc
Aur3 Admin <admin@lablz.com>
2013-02-23 14:45:15 -0500

    fork me

--- js/views/PublishButton.js
+++ js/views/PublishButton.js
@@ -10,18 +10,28 @@ define([
   return BasicView.extend({
     template: 'publishButtonTemplate',
     events: {
-      'click #publish': 'publish'
+      'click #publish': 'publish',
+      'click #try': 'tryApp',
+      'click #fork': 'forkApp'
     },
     initialize: function(options) {
-      _.bindAll(this, 'render', 'publish');
+      _.bindAll(this, 'render', 'publish', 'tryApp');
       this.constructor.__super__.initialize.apply(this, arguments);
       this.template = _.template(Templates.get(this.template));
+      this.tryTemplate = _.template(Templates.get('tryButtonTemplate'));
+      this.forkTemplate = _.template(Templates.get('forkButtonTemplate'));
       return this;
+    },
+    tryApp: function(e) {
+      Events.stopEvent(e);
+      var res = this.resource;
+      window.location.href = G.serverName + '/app/' + res.get('appPath');      
+//      this.router.navigate('app/' + res.get('appPath'), {trigger: true});
     },
     publish: function(e) {
       Events.stopEvent(e);
       var res = this.resource;
-      var props = {deploy: true};
+      var props = {publish: true};
       var self = this;
       res.save(props, {
         sync: true,
@@ -36,13 +46,30 @@ define([
         window.location.reload();
         }
 //      ,
-//        queryString: 'deploy=true'
+//        queryString: 'publish=true'
 //        error: onSaveError
       });
       return this;
     },
-    render: function() {
-      if (this.template) {
+    forkApp: function(e) {
+      Events.stopEvent(e);
+      var res = this.resource;
+//      window.location.href = G.serverName + '/app/' + res.get('appPath');      
+      this.router.navigate('make/model%2Fsocial%2FApp?basedOnTemplate='  + encodeURIComponent(res.get('_uri')), {trigger: true});
+    },
+
+    render: function(options) {
+      if (options) {
+        if (options.forkMe) {
+          this.$el.html(this.forkTemplate());
+          this.$el.trigger('create');
+        }
+        if (options  &&  options.tryApp) {
+          this.$el.html(this.tryTemplate());
+          this.$el.trigger('create');
+        }
+      }
+      else if (this.template) {
         this.$el.html(this.template());
         this.$el.trigger('create');
       }
