0519177b49719ea0897cc551c9a5864a7cf2b31b
Aur3 Admin <admin@lablz.com>
2013-02-23 14:44:43 -0500

    handler for buy/try button on choose Buyable chooser RL

--- js/views/ListPage.js
+++ js/views/ListPage.js
@@ -20,7 +20,7 @@ define([
     template: 'resource-list',
     clicked: false,
     initialize: function(options) {
-      _.bindAll(this, 'render', 'home', 'submit', 'swipeleft', 'swiperight', 'pageshow', 'pageChanged', 'setMode');
+      _.bindAll(this, 'render', 'home', 'submit', 'swipeleft', 'click', 'swiperight', 'pageshow', 'pageChanged', 'setMode');
       this.constructor.__super__.initialize.apply(this, arguments);
       Events.on('changePage', this.pageChanged);
       this.template = _.template(Templates.get(this.template));
@@ -37,7 +37,7 @@ define([
         this.listView.setMode(mode);
     },
     events: {
-//      'click': 'click',
+      'click': 'click',
       'click #nextPage': 'getNextPage',
       'click #homeBtn': 'home',
       'swiperight': 'swiperight',
@@ -93,10 +93,44 @@ define([
   //    Events.trigger('nextPage', this.resource);    
   //  },
 //    tap: Events.defaultTapHandler,
-//    click: function(e) {
-//      clicked = true;
-//      Events.defaultClickHandler(e);  
-//    },
+    click: function(e) {
+      clicked = true;
+      var buyLink;
+      var tryLink;
+      if (!U.isA(this.vocModel, 'Buyable') || ((buyLink = $(e.target).closest($('#buyLink'))).length == 0  &&  (tryLink = $(e.target).closest($('#tryLink'))).length == 0)) {
+//        Events.defaultClickHandler(e);
+        return true;
+      }
+
+      Events.stopEvent(e);
+      
+      var uri = buyLink.length ? $(buyLink[0]).attr('href') :  $(tryLink[0]).attr('href');
+      var models = this.model.models;
+      var res = $.grep(models, function(item) {
+        return item.getUri() == uri;
+      })[0];
+      if (!buyLink.length) {
+        Events.trigger('chooser', {model: res, buy: true});
+        return;
+      }
+      var newRes = new G.typeToModel[this.vocModel.type]();
+      var p = U.getCloneOf(this.vocModel, 'Buyable.template');
+      var props = {};
+      props[p[0]] = uri;
+      newRes.save(props, {
+        success: function(resource, response, options) {
+          if (response.error) {
+            onSaveError(resource, response, options);
+            return;
+          }
+          
+          res.lastFetchOrigin = null;
+          self.redirect(res, {trigger: true, replace: true, forceRefresh: true, removeFromView: true});
+        }
+        
+      });
+
+    },
     
     render:function (eventName) {
       G.log(this.TAG, 'render');  
@@ -104,10 +138,15 @@ define([
       
       var json = rl.toJSON();
       json.viewId = this.cid;
+      var vocModel = this.vocModel;
+      var viewMode = vocModel.viewMode;
+      var isList = this.isList = (typeof viewMode != 'undefined'  &&  viewMode == 'List');
+      var isChooser = window.location.hash  &&  window.location.hash.indexOf('#chooser/') == 0;  
+      var isMasonry = this.isMasonry = !isChooser  &&  (vocModel.type.endsWith('/Theme') || vocModel.type.endsWith('/App') || vocModel.type.endsWith('/Goal') || vocModel.type.endsWith('/ThirtyDayTrial')); //  ||  vocModel.type.endsWith('/Vote'); //!isList  &&  U.isMasonry(vocModel); 
+      _.extend(json, {isMasonry: isMasonry});  
       this.$el.html(this.template(json));
       
       var isGeo = (rl.isOneOf(["Locatable", "Shape"])) && _.any(rl.models, function(m) {return !_.isUndefined(m.get('latitude')) || !_.isUndefined(m.get('shapeJson'))});
-      var vocModel = this.vocModel;
       var hash = window.location.hash;
       var idx;
       var isChooser = window.location.hash  &&  window.location.hash.indexOf('#chooser/') == 0;
@@ -160,10 +199,6 @@ define([
 //      meta = meta || models[0].properties;
       var meta = vocModel.properties;
 
-      var viewMode = vocModel.viewMode;
-      var isList = this.isList = (typeof viewMode != 'undefined'  &&  viewMode == 'List');
-      var isChooser = window.location.hash  &&  window.location.hash.indexOf('#chooser/') == 0;  
-      var isMasonry = this.isMasonry = !isChooser  &&  (vocModel.type.endsWith('/App') || vocModel.type.endsWith('/Goal') || vocModel.type.endsWith('/ThirtyDayTrial')); //  ||  vocModel.type.endsWith('/Vote'); //!isList  &&  U.isMasonry(vocModel); 
 //      var isMasonry = this.isMasonry = !isList && U.isA(vocModel, 'ImageResource')  &&  (U.getCloneOf(vocModel, 'ImageResource.mediumImage').length > 0 || U.getCloneOf(vocModel, 'ImageResource.bigMediumImage').length > 0  ||  U.getCloneOf(meta, 'ImageResource.bigImage').length > 0);
 //      if (isMasonry) {
 //        var key = this.vocModel.shortName + '-list-item';
