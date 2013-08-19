66d013d705ce83c06cf9b5c8ded3856b862962ae
Mark Vayngrib <mark.vayngrib@lablz.com>
2013-08-19 23:16:23 +0800

    added static page support

---
+++ js/views/StaticPage.js
@@ -0,0 +1,42 @@
+//'use strict';
+define('views/StaticPage', [
+  'globals',
+  'utils',
+  'events',
+  'views/BasicPageView',
+  'views/Header',
+  'vocManager',
+  'collections/ResourceList'
+], function(G, U, Events, BasicPageView, Header, Voc, ResourceList) {
+  return BasicPageView.extend({
+    initialize: function(options) {
+      options = options || {};
+      this.constructor.__super__.initialize.apply(this, arguments);
+
+      if (options.header) {
+        this.headerButtons = {
+          back: true,
+          menu: true,
+          rightMenu: !G.currentUser.guest,
+          login: G.currentUser.guest
+        };
+        
+        this.addChild('header', new Header({
+          viewId: this.cid,
+          parentView: this,
+          model: this.model
+        }));
+      }
+
+      this.makeTemplate(options.template || U.getCurrentUrlInfo().params.template, 'template');
+    },
+    
+    render: function() {
+      this.$el.html(this.template());
+      if (!this.$el.parentNode) 
+        $('body').append(this.$el);
+    }    
+  }, {
+    displayName: 'StaticPage'
+  });
+});
