8f018581e99037e10a43deb2897064f0627e7f0a
Mark Vayngrib <mark.vayngrib@lablz.com>
2013-10-17 21:09:29 +0800

    adapting masonry to new scroller1

--- js/views/ResourceMasonryItemView.js
+++ js/views/ResourceMasonryItemView.js
@@ -114,7 +114,7 @@ define('views/ResourceMasonryItemView', [
       var html = this.template(data);
       if (options && options.renderToHtml) {
         var tagName = this.tagName || 'div';
-        this._html = '<{0}>{1}</{0}>'.format(tagName, html);
+        this._html = '<{0} class="{1}">{2}</{0}>'.format(tagName, this.className, html);
         return this;
       }
       else
