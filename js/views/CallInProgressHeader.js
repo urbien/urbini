a9f9b224dca4cd2b49b45ff14551455b4df9bacc
Ellen Katsnelson <ellen.katsnelson@lablz.com>
2013-11-17 01:09:57 -0500

    minor change

--- js/views/CallInProgressHeader.js
+++ js/views/CallInProgressHeader.js
@@ -8,7 +8,7 @@ define('views/CallInProgressHeader', [
   return BasicView.extend({
     initialize: function(options) {
       _.bindAll(this, 'render', 'backToCall', 'hangUp', 'sendToCall');
-      this.constructor.__super__.initialize.apply(this, arguments);
+      BasicView.prototype.initialize.apply(this, arguments);
       options = options || {};
     
       var self = this;
