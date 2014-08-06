d36c6861e436601c8a3863b0517a3f84ee0aeab2
Mark Vayngrib <mark.vayngrib@lablz.com>
2014-08-06 13:59:19 -0400

    added historicalDataView

--- js/views/EditPage.js
+++ js/views/EditPage.js
@@ -20,14 +20,27 @@ define('views/EditPage', [
     initialize: function(options) {
       _.bindAll(this, 'render', 'edit', 'home', 'set', 'resetForm');
       BasicPageView.prototype.initialize.apply(this, arguments);
-      
+
   //    this.resource.on('change', this.render, this);
       this.makeTemplate('resourceEdit', 'template', this.vocModel.type);
       this.editOptions = _.extend({action: 'edit'}, _.pick(options, editParams));
       _.extend(this, this.editOptions);
       this.listenTo(Events, "mapReady", this.showMapButton);
 
-      var res = this.resource;
+      var self = this,
+          res = this.resource,
+          type = this.vocModel.type;
+
+      this.hasHistoricalData = res.isAssignableFrom('commerce/trading/NumericRule');
+      if (this.hasHistoricalData) {
+        U.require('views/HistoricalDataView').done(function(HDV) {
+          self.historicalDataView = new HDV({model: res, parentView: self});
+          self.addChild(self.historicalDataView);
+          if (self.rendered)
+            self.renderHistoricalData();
+        });
+      }
+
   //    var json = res.toJSON();
   //    json.viewId = this.cid;
       this.settings = {viewId: this.cid};
@@ -37,7 +50,7 @@ define('views/EditPage', [
         this.editOptions.saveOnBack = false;
 //        settings.noCancel = true;
       }
-      
+
       var isGeo = this.isGeo();
       this.buttons = {
 //        back: true,
@@ -48,18 +61,18 @@ define('views/EditPage', [
 //        login: G.currentUser.guest,
 //        rightMenu: !G.currentUser.guest
       };
-    
+
       this.header = new Header({
-        model: res, 
-  //      pageTitle: this.pageTitle || res.get('davDisplayName'), 
+        model: res,
+  //      pageTitle: this.pageTitle || res.get('davDisplayName'),
         buttons: this.buttons,
         viewId: this.cid,
         parentView: this,
         isEdit: true
       });
-      
+
       this.addChild(this.header);
-      
+
       var reqParams = _.getParamMap(window.location.href);
       var editCols =  reqParams['$editCols'];
       this.isVideo = res.isA('VideoResource');
@@ -67,24 +80,24 @@ define('views/EditPage', [
         this.imageView = new ResourceImageView({model: res, parentView: this});
         this.addChild(this.imageView);
       }
-      
+
       this.editView = new EditView(_.extend({model: res, parentView: this}, this.editOptions));
       this.addChild(this.editView);
       if (this.editParams)
         this.editView.set(this.editParams);
     },
-    
+
 //    shortcut: function() {
 //      U.hasUnsetEditableProperties(this.resource);
 //      var meta = this.resource.vocModel.properties,
 //          atts = this.resource.attributes,
 //          numUnset = 0;
-//      
+//
 //      for (var props in meta) {
 //        if (meta[])
 //      }
 //    },
-    
+
     set: function(params) {
       _.extend(this, params);
       if (this.editView)
@@ -95,12 +108,19 @@ define('views/EditPage', [
     events: {
       'click #edit'   : 'edit',
 //      'click': 'click',
-      'click #homeBtn': 'home'
+      'click #homeBtn': 'home',
+      'click .historicalData [data-value]': 'onChoseHistoricalValue'
     },
-    
+
     myEvents: {
       'userSaved': 'submit',
       'userCanceled': 'cancel'
+    },
+
+    onChoseHistoricalValue: function(e) {
+      var input = this.$('input[type="text"]')[0];
+      if (input)
+        input.value = e.selectorTarget.$data('value');
     },
 
     submit: function() {
@@ -127,35 +147,38 @@ define('views/EditPage', [
     },
 
     render: function(options) {
-      if (!this.rendered) 
+      if (!this.rendered)
         this.addToWorld(null, true);
-      
+
       var self = this;
       this.getFetchPromise().done(function() {
         self.renderHelper(options);
         self.finish();
       });
     },
-    
+
     renderHelper: function(options) {
       this.el.$html(this.template(this.settings));
       var views = {
         '#resourceEditView': this.editView,
         '.headerDiv'       : this.header
       };
-      
+
 //      if (this.imageView)
 //        views['div#resourceImage'] = this.imageView;
 
-      this.assign(views);      
-//      if (G.theme.backgroundImage) 
+      this.assign(views);
+      if (this.historicalDataView)
+        this.renderHistoricalData();
+
+//      if (G.theme.backgroundImage)
 //        this.$('#resourceEditView').$css('background-image', 'url(' + G.theme.backgroundImage +')');
 
       // Comments inline
       var isComment = U.isAssignableFrom(this.vocModel, U.getLongUri1("model/portal/Comment"));
-      if (!isComment) 
+      if (!isComment)
         return this;
-      
+
       var self = this;
       var ranges = [];
       ranges.push(this.vocModel.type);
@@ -176,7 +199,7 @@ define('views/EditPage', [
             self.commentsView = new ResourceListView({model: inlineList, parentView: self, el: this.$('#comments')[0]});
             self.addChild(self.commentsView);
             self.assign('#comments', self.commentsView);
-              
+
 //              _.each(['updated', 'added', 'reset'], function(event) {
 //                self.stopListening(inlineList, event);
 //                self.listenTo(inlineList, event, function(resources) {
@@ -196,10 +219,16 @@ define('views/EditPage', [
         }
       // end Comments
       });
-      
+
       return this;
     },
-    
+
+    renderHistoricalData: function() {
+      this.assign({
+        '.historicalData': this.historicalDataView
+      });
+    },
+
     isSubmitted: function() {
       return this.editView.isSubmitted();
     }
---
+++ js/views/HistoricalDataView.js
@@ -0,0 +1,215 @@
+define('views/HistoricalDataView', [
+  'globals',
+  'utils',
+  'views/BasicView',
+  'vocManager',
+  'collections/ResourceList'
+], function(G, U, BasicView, Voc, ResourceList) {
+
+  var RawValue = G.DEV_PACKAGE_PATH + 'Technicals/RawValue';
+  var PreviousValue = G.DEV_PACKAGE_PATH + 'Technicals/PreviousValue';
+
+  function getVariantUri(indicator) {
+    var variantUri = indicator.get('variantUri');
+    if (variantUri == RawValue || variantUri == PreviousValue) {
+      variantUri = indicator.get('eventPropertyUri');
+      variantUri = variantUri.slice(0, variantUri.lastIndexOf('/'));
+    }
+
+    return variantUri;
+  }
+
+  function getEventPropertyName(indicator) {
+    var uri = indicator.get('eventPropertyUri');
+    return uri.slice(uri.lastIndexOf('/') + 1);
+  }
+
+  function getCols(listModel, indicator) {
+    var cols,
+        meta = listModel.properties,
+        date,
+        value;
+
+    if (U.isAssignableFrom(listModel, 'commerce/trading/Event')) {
+      date = U.getSubpropertyOf(listModel, 'dateOccurred');
+      if (!date)
+        return null;
+
+      value = meta[getEventPropertyName(indicator)];
+    }
+    else {
+      date = meta.date;
+      value = meta.value;
+    }
+
+    return date && value && {
+      date: date,
+      value: value
+    };
+  }
+
+  function toTable(list, indicator, cols) {
+    return {
+      heading: U.getDisplayName(indicator).replace('Previous Value', '').trim(),
+      cols: cols,
+      resources: list.models
+    }
+  };
+
+  return BasicView.extend({
+    autoFinish: false,
+    template: 'historicalDataTemplate',
+    initialize: function(options) {
+      _.bindAll(this, 'render', 'renderHelper');
+      BasicView.prototype.initialize.apply(this, arguments);
+      this.makeTemplate(this.template, 'template', this.vocModel.type);
+    },
+
+    events: {
+      'click th[data-shortname]': 'sortBy'
+    },
+
+    sortBy: function(e) {
+      var shortname = e.selectorTarget.$data('shortname'),
+          indicator = e.selectorTarget.$closest('table').$data('indicator'),
+          table = this.historicalDataTables[indicator];
+
+      this._sortBy(table, shortname);
+    },
+
+    _sortBy: function(table, shortname) {
+      if (table.order == shortname) {
+        table.resources.reverse();
+      }
+      else {
+        table.order = shortname;
+        // this.historicalData.sortBy(this._sortBy);
+        table.resources = _.sortBy(table.resources, function(obj) { return obj[shortname]; });
+      }
+
+      this.render();
+    },
+
+    loadHistoricalData: function() {
+      if (this._historicalDataPromise)
+        return this._historicalDataPromise;
+
+      // var self = this;
+      // return this._historicalDataPromise = $.Deferred(function(dfd) {
+      //   var now = +new Date(),
+      //       value = 1000;
+
+      //   self.historicalData = [];
+      //   for (var i = 0; i < 100; i++) {
+      //     self.historicalData.push({
+      //       date: U.getFormattedDate1(new Date(now -= 24 * 3600 * 1000)),
+      //       value: value += (Math.random() * 50 - 25)
+      //     });
+      //   }
+
+      //   dfd.resolve();
+      // }).promise();
+
+      var self = this,
+          res = this.resource,
+          indicatorUri = res.get('indicator'),
+          compareWithUri = res.get('compareWith'),
+          promises = [U.getResourcePromise(indicatorUri)],
+          dfd = $.Deferred();
+
+      if (compareWithUri)
+        promises.push(U.getResourcePromise(compareWithUri));
+
+      $.when.apply($, promises).done(function(indicator, compareWith) {
+        self.indicators = {};
+        var variants = [];
+        for (var i = 0; i < arguments.length; i++) {
+          var indicator = arguments[i];
+          if (indicator) {
+            var variantUri = getVariantUri(indicator);
+            if (variants.indexOf(variantUri) == -1) {
+              self.indicators[indicator.getUri()] = indicator;
+              variants.push(variantUri);
+            }
+          }
+        }
+
+        if (!variants.length)
+          dfd.reject();
+
+        Voc.getModels(variants).done(function() {
+          self.historicalDataLists = {};
+          self.historicalDataTables = {};
+          _.map(self.indicators, function(indicator, uri) {
+            var model = U.getModel(getVariantUri(indicator));
+            if (!model)
+              return;
+
+            var cols = getCols(model, indicator);
+            if (!cols)
+              return;
+
+            var dateProp = cols.date.shortName,
+                valProp = cols.value.shortName,
+                params = {
+                  $orderBy: dateProp,
+                  $select: dateProp + ',' + valProp,
+                  $asc: false
+                };
+
+            params[valProp] = '!null';
+
+            var list = new ResourceList(null, {
+              model: model,
+              params: params
+            });
+
+            list.fetch({
+              success: function() {
+                if (list.length) {
+                  self.historicalDataLists[uri] = list;
+                  var table = toTable(list, indicator, cols);
+                  if (table) {
+                    self.historicalDataTables[uri] = table;
+                    table.order = dateProp;
+                    dfd.notify();
+                  }
+                }
+              }
+            });
+          });
+        });
+      });
+
+      return (this._historicalDataPromise = dfd.promise());
+    },
+
+    render: function() {
+      this.loadHistoricalData().progress(this.renderHelper); // repaint when new data arrives
+    },
+
+    renderHelper: function() {
+      // var list = this.historicalData.models,
+      //     model = this.historicalData.vocModel,
+      //     meta = model.properties,
+      //     l = list.length,
+      //     props,
+      //     data;
+
+      // if (!l || !dataEl)
+      //   return;
+
+      // dataEl.$html(this.historicalDataTemplate({ cols: props.map(function(p) { return meta[p]; }), resources: models }));
+
+      this.html(this.template({
+        tables: this.historicalDataTables
+      }));
+
+      this.getPageView().invalidateSize();
+      this.finish();
+    }
+  }, {
+    displayName: 'HistoricalDataView'
+  });
+});
+
