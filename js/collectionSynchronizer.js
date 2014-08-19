d303c343a57887dd9a76aab5e79445f7a3868ee6
Mark Vayngrib <mark.vayngrib@lablz.com>
2014-08-19 11:38:27 -0400

    added comment

--- js/collectionSynchronizer.js
+++ js/collectionSynchronizer.js
@@ -7,19 +7,19 @@ define('collectionSynchronizer', ['globals', 'underscore', 'utils', 'synchronize
   function CollectionSynchronizer() {
     Synchronizer.apply(this, arguments);
   };
-  
+
   function addEmptyResponseHeaderFn(xhr) {
     xhr.getResponseHeader = G.emptyFn;
     return xhr;
   }
-  
+
   CollectionSynchronizer.prototype = Object.create(Synchronizer.prototype);
   CollectionSynchronizer.constructor = CollectionSynchronizer;
 
   CollectionSynchronizer.prototype._getLastFetchedOn = function() {
     if (this.info && !_.isUndefined(this.info.lastFetchedOn))
       return this.info.lastFetchedOn;
-    
+
     var resources = this.data.models;
     return resources.length && resources[0].loaded && Synchronizer.getLastFetched(this.data.models, this._getNow());
   };
@@ -27,7 +27,7 @@ define('collectionSynchronizer', ['globals', 'underscore', 'utils', 'synchronize
   CollectionSynchronizer.prototype._queryDB = function() {
     var filter = U.getQueryParams(this.data),
         meta = this.data.vocModel.properties;
-    
+
     if (_.any(_.keys(filter), function(param) { // no support for searching by compound params like a.b yet
       return /\./.test(param);
     })) {
@@ -42,24 +42,24 @@ define('collectionSynchronizer', ['globals', 'underscore', 'utils', 'synchronize
       filter: filter
     });
   };
-  
+
   CollectionSynchronizer.prototype.canFetchFromServer = function() {
     var params = this.data.params,
         meta = this.data.vocModel;
-    
+
     for (var p in params) {
       var prop = meta[p];
       if (prop && U.isResourceProp(p) && U.isTempUri(params[p]))
         return false
     }
-    
+
     return true;
   },
-  
+
 //  CollectionSynchronizer.prototype._fetchFromServer = function() {
 //    if (!this.data.isUpdate())
 //  };
-  
+
   CollectionSynchronizer.prototype._preProcess = function() {
     var result = Synchronizer.prototype._preProcess.call(this),
         params = this.data.params,
@@ -69,7 +69,7 @@ define('collectionSynchronizer', ['globals', 'underscore', 'utils', 'synchronize
         info = this.info,
         numRequested,
         shortPage;
-    
+
     if (options.from)
       info.start = params.$offset && parseInt(params.$offset) || 0;
     else
@@ -77,19 +77,19 @@ define('collectionSynchronizer', ['globals', 'underscore', 'utils', 'synchronize
 
     numRequested = params.$limit ? parseInt(params.$limit) : perPage;
     info.end = info.start + numRequested;
-    
+
 //    shortPage = !!(numNow && numNow < perPage);
     info.isUpdate = info.start < numNow; //numNow >= info.end || ; // || shortPage;
 //    if (numNow) // we've already gotten everything out of the DB
 //      this.info.isForceFetch = true;
-    
+
     return result;
   };
-  
+
   CollectionSynchronizer.prototype._read = function() {
     if (!this._preProcess())
       return;
-    
+
     if (this._isUpdate()) {
       if (this._isForceFetch() || this._isStale())
         return this._delayedFetch(); // shortPage ? null : lf); // if shortPage, don't set If-Modified-Since header
@@ -105,27 +105,27 @@ define('collectionSynchronizer', ['globals', 'underscore', 'utils', 'synchronize
         this._error(null, {code: 204, details: 'End of list'}, this.options);
         return;
       }
-      
-      if (this._isStale() && this.info.start < this.data.length) { 
+
+      if (this._isStale() && this.info.start < this.data.length) {
         // we've got some resources at the end of an incomplete page, meaning there's nothing to fetch from the server
         this._success(null, 'success', addEmptyResponseHeaderFn({status: 304})); // no need to refetch from db, we already did, and there's nothing to fetch from the server it seems
-        return; 
+        return;
       }
     }
-    
+
     var adapter = this.data.vocModel.adapter;
     if (adapter && adapter.supportsPaging && !adapter.supportsPaging()) {
       this._error(null, {code: 204, details: 'End of list'}, this.options);
       return;
     }
-    
+
     return Synchronizer.prototype._read.call(this);
   };
-  
+
   CollectionSynchronizer.prototype._onDBSuccess = function(results) {
     if (!results || !results.length)
       return this._fetchFromServer(100);
-          
+
     var info = this.info,
         numBefore = this.data.length,
         numAfter,
@@ -134,7 +134,7 @@ define('collectionSynchronizer', ['globals', 'underscore', 'utils', 'synchronize
           offset: this.options.start
         };
 //        resp = {
-//          data: results, 
+//          data: results,
 //          metadata: {
 //            offset: this.options.start
 //          }
@@ -145,12 +145,12 @@ define('collectionSynchronizer', ['globals', 'underscore', 'utils', 'synchronize
     try {
       if (this._isForceFetch())
         return this._fetchFromServer();
-          
+
       numAfter = this.data.length;
       info.isUpdate = info.isUpdate || numAfter >= info.end; // || shortPage;
       if (!info.isUpdate && numAfter === numBefore) // db results are useless
         return this._delayedFetch();
-      
+
       lastFetchedTS = Synchronizer.getLastFetched(results, this._getNow());
       if (this._isStale(lastFetchedTS, this._getNow()))
         return this._delayedFetch();
@@ -167,7 +167,7 @@ define('collectionSynchronizer', ['globals', 'underscore', 'utils', 'synchronize
   CollectionSynchronizer.prototype._getItems = function(options) {
     if (NO_DB)
       return REJECTED_PROMISE;
-    
+
     var self = this,
         IDB = IndexedDBModule.getIDB(),
         type = U.getTypeUri(options.key),
@@ -177,7 +177,7 @@ define('collectionSynchronizer', ['globals', 'underscore', 'utils', 'synchronize
         props = data.vocModel.properties,
         temps = {},
         query;
-    
+
     if (!IDB.hasStore(type))
       return REJECTED_PROMISE;
 
@@ -187,23 +187,24 @@ define('collectionSynchronizer', ['globals', 'underscore', 'utils', 'synchronize
     })) {
       return REJECTED_PROMISE;
     }
-          
+
     for (var key in filter) {
       var val = filter[key],
           prop = props[key];
-      
+
       if (prop && U.isResourceProp(prop) && U.isTempUri(val)) {
         temps[key] = val;
       }
     }
-    
+
     if (_.isEmpty(temps)) {
       var search = function() {
         options = _.clone(options);
         options.filter = data.belongsInCollection;
+        // can't do orderBy, as we have no index for this property, if we're searching
         return IDB.search(type, options);
       };
-      
+
       query = QueryBuilder.buildQuery(data, filter);
       if (query) {
         return IDB.queryByIndex(query).getAll(type).then(function(results) {
@@ -213,11 +214,11 @@ define('collectionSynchronizer', ['globals', 'underscore', 'utils', 'synchronize
       else
         return search();
     }
-    
+
     return IDB.queryByIndex('_tempUri').oneof(_.values(temps)).getAll(REF_STORE.name).then(function(results) {
       if (!results.length)
         return REJECTED_PROMISE;
-      
+
       var tempUriToRef = {};
       for (var i = 0; i < results.length; i++) {
         var r = results[i];
@@ -225,7 +226,7 @@ define('collectionSynchronizer', ['globals', 'underscore', 'utils', 'synchronize
           tempUriToRef[r._tempUri] = r._uri;
         }
       }
-      
+
       for (var key in temps) {
         var tempUri = temps[key];
         var ref = tempUriToRef[tempUri];
@@ -233,21 +234,21 @@ define('collectionSynchronizer', ['globals', 'underscore', 'utils', 'synchronize
           filter[key] = ref._uri;
         }
       }
-      
+
       return self._getItems(options);
     }, function() {
       debugger;
     });
   };
-  
+
   CollectionSynchronizer.prototype._getKey = function() {
     return this.data.vocModel.type;
   };
-  
+
   CollectionSynchronizer.init = function() {
     REF_STORE = G.getRefStoreInfo();
     delete CollectionSynchronizer.init;
   };
-  
+
   return CollectionSynchronizer;
 });
