c20f9c52d1b37b0d4a70406ee19efe2df29650c3
Mark Vayngrib <mark.vayngrib@lablz.com>
2014-08-25 12:23:41 -0400

    remove tooltips fix

--- js/views/BasicPageView.js
+++ js/views/BasicPageView.js
@@ -589,9 +589,13 @@ define('views/BasicPageView', [
 
     _fixTooltips: function() {
       var self = this,
-          remove = [];
+          remove = [],
+          tooltips = this.getTooltips();
 
-      this.getTooltips().$forEach(function(tooltip) {
+      if (!tooltips)
+        return;
+
+      tooltips.$forEach(function(tooltip) {
         var el = self._getTooltipBaseElement(tooltip);
         if (!el) {
           remove.push(tooltip);
@@ -622,7 +626,7 @@ define('views/BasicPageView', [
     },
 
     getTooltips: function() {
-      return this.el.$('.play');
+      return this.el && this.el.$('.play');
     },
 
     removeTooltips: function() {
@@ -669,15 +673,16 @@ define('views/BasicPageView', [
       page.$prepend(tooltipEl);
 
       var events = ['tap', 'drag'];
+      function removeTooltips() {
+        events.map(function(event) {
+          document.$off(event, removeTooltips, true);
+        });
+
+        self.removeTooltips();
+      };
+
       events.map(function(event) {
-        function removeTooltips() {
-          events.map(function(event) {
-            document.$off(event, removeTooltips, true);
-          });
-
-          self.removeTooltips();
-        };
-
+        removeTooltips._id = G.nextId();
         document.$on(event, removeTooltips, true);
         self.on('destroyed', document.$off.bind(document, removeTooltips));
       });
